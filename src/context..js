6a4264850f922e1071c27211b34086b8edc86e8a
Diljot Singh <89629746+diljotsingh04@users.noreply.github.com>
2023-06-16 20:50:20 +0530

    Update context..js

--- src/context..js
+++ src/context..js
@@ -1,7 +1,7 @@
 import React, {useState, useContext, useEffect} from 'react';
 import { useCallback } from 'react';
 const URL = "http://openlibrary.org/search.json?title=";
-const AppContext = React.createContext();
+const AppContext = React.createContext(); 
 
 const AppProvider = ({children}) => {
     const [searchTerm, setSearchTerm] = useState("the lost world");
